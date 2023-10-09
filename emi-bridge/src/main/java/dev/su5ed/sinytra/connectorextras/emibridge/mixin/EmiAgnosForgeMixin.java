6d9229fa9b0f267c9f2a711fee686c2000c75f65
Su5eD <su5ed@outlook.com>
2023-10-09 20:29:54 +0200

    Prevent EMI plugins from being loaded twice
    Fixes #5

--- emi-bridge/src/main/java/dev/su5ed/sinytra/connectorextras/emibridge/mixin/EmiAgnosForgeMixin.java
+++ emi-bridge/src/main/java/dev/su5ed/sinytra/connectorextras/emibridge/mixin/EmiAgnosForgeMixin.java
@@ -9,14 +9,18 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mixin(EmiAgnosForge.class)
 public class EmiAgnosForgeMixin {
     @ModifyVariable(method = "getPluginsAgnos", at = @At("RETURN"), remap = false)
     private List<EmiPluginContainer> connectorextras_emi_bridge$addFabricPlugins(List<EmiPluginContainer> orig) {
+        Set<String> ids = orig.stream().map(EmiPluginContainer::id).collect(Collectors.toSet());
         orig.addAll(FabricLoader.getInstance()
             .getEntrypointContainers("emi", EmiPlugin.class)
             .stream()
+            .filter(plugin -> !ids.contains(plugin.getProvider().getMetadata().getId()))
             .map(p -> new EmiPluginContainer(p.getEntrypoint(), p.getProvider().getMetadata().getId()))
             .toList());
         return orig;
